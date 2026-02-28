820819564187977bb2883d173e498797779f2c8a
shvkmr536 <shvkmr536@gmail.com>
2026-03-01 03:24:57 +0530

    updates variables

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "demo" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = var.subnet_id
-  vpc_security_group_ids = var.security_group_ids
+  #subnet_id     = var.subnet_id
+  #vpc_security_group_ids = var.security_group_ids
   tags = {
     Name        = "web-server-${var.env}"
     Environment = var.env
