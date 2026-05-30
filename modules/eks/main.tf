8a122bdfe4243ec3af73521262e56996412388ec
shvkmr536 <shvkmr536@gmail.com>
2026-05-31 02:17:17 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -8,7 +8,10 @@ resource "aws_eks_cluster" "eks" {
   "audit",
   "authenticator"
 ]
-
+  access_config {
+     authentication_mode                         = "API_AND_CONFIG_MAP"
+     bootstrap_cluster_creator_admin_permissions = true
+  }
   vpc_config {
     subnet_ids = var.private_subnets
   }
